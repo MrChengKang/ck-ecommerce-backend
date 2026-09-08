59bec0a7364da0d58137a9b2cc60e4c57d8beada
CK <chengkanghow0715@gmail.com>
2026-09-08 10:27:13 +0800

    Add CrossOrigin to AuthController

--- src/main/java/com/ckang/ecommerce_backend/AuthController.java
+++ src/main/java/com/ckang/ecommerce_backend/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
   @Autowired
